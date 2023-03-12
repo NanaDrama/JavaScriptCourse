7668e0bd891e8c0ec45df39b44c071e774a0874d
NanaDrama <123100389+NanaDrama@users.noreply.github.com>
2023-03-12 20:45:27 +0200

    add task 6

--- js/homework2.js
+++ js/homework2.js
@@ -46,3 +46,14 @@ if(ta ** 2 == tc ** 2 + tb ** 2 || tc ** 2 == ta ** 2 + tb ** 2 || tb ** 2 == ta
     console.log('Трикутник не прямокутний')
 }
 /* task 6 */
+const now = new Date();
+const hour  = now.getHours();
+if(hour >= 23 && hour <= 5){
+    console.log('Доброї ночі')
+}if(hour > 5 && hour <= 11){
+    console.log('Доброго ранку')
+}if(hour > 11 && hour <= 17){
+    console.log('Доброго дня')
+}else{
+    console.log('Доброго вечора')
+}
