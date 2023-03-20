d3d9b260b3ef28f8eacbe1e6aac3fedf8677a470
NanaDrama2 <127668482+NanaDrama2@users.noreply.github.com>
2023-03-20 20:40:07 +0200

    Update homework3.js

--- js/homework3.js
+++ js/homework3.js
@@ -1,9 +1,11 @@
 /* task 1 */
 function compact(arr) {
     let result = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (!result.includes(arr[i])) {
-        result.push(arr[i]);
+    for (let i = 0; i < arr.length; i+=1) {
+      if (result.includes(arr[i])) {
+        continue;
+      }else{
+          result.push(arr[i]);
       }
     }
     return result;
